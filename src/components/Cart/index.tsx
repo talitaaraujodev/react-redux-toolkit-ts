cc9f930dad6fa6a0c78914b5d169d976eef832f5
Talita Araujo <74464709+talitaaraujodev@users.noreply.github.com>
2024-04-05 14:36:26 -0300

    Adc component CartEmpty

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -4,23 +4,28 @@ import { CartItem } from "../CartItem";
 import { Product } from "../../redux/cart/cartSlice";
 import { selectProductsTotalPrice } from "../../redux/cart/cartSelector";
 import { utils } from "../../utils";
+import { CartEmpty } from "../CartEmpty";
 
 export function Cart() {
   const { products } = useSelector((state: RootState) => state.cart);
   const productsTotale = useSelector(selectProductsTotalPrice);
   return (
-    <div className="bg-white h-full w-64 z-10 fixed top-0 right-0 flex flex-col shadow-sm border-l overflow-y-auto">
+    <div className="bg-white h-full w-64 z-10 fixed top-0 right-0 flex flex-col shadow-sm border-l-2 overflow-y-auto px-2 py-1">
       <h2 className="text-xl p-2 font-semibold border-b text-[#00b4ed]">
         Meu Carrinho
       </h2>
       <div className="flex flex-col">
-        {products.map((product: Product) => (
-          <CartItem
-            product={product}
-            imgLocale={product.imgUrl}
-            key={product.id}
-          />
-        ))}
+        {products.length > 0 ? (
+          products.map((product: Product) => (
+            <CartItem
+              product={product}
+              imgLocale={product.imgUrl}
+              key={product.id}
+            />
+          ))
+        ) : (
+          <CartEmpty />
+        )}
       </div>
 
       <div className="flex justify-between items-center px-3 py-2 mt-auto">
---
+++ src/components/CartEmpty/index.tsx
@@ -0,0 +1,7 @@
+export function CartEmpty() {
+  return (
+    <h2 className="text-lg font-normal text-center text-[#282828] mt-3">
+      Carrinho vazio, continue navegando e adicione produtos aqui!
+    </h2>
+  );
+}
